639757ff0c5b5f720c3fc24c1b7713d48f7fdd6e
RodrigoGasparSeptiembre <92555965+RodrigoGasparSeptiembre@users.noreply.github.com>
2021-12-16 20:31:21 -0300

    Add files via upload

--- models.py
+++ models.py
@@ -6,8 +6,8 @@ class persona(models.model):
     nombre= models.charfield(max_lenght=60)
     apellido= models.charfield(max_lenght=100)
     club= models.charfield(max_lenght=50)
-    class Meta:
-        verbose_name= ('Persona')
-        verbose_name_plural= ('Personas')
-        def __str__(self):
-            return self.nombre
+class Meta:
+    verbose_name= ('Persona')
+    verbose_name_plural= ('Personas')
+    def __str__(self):
+    return self.nombre
