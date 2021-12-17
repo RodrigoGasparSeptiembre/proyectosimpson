8f0e4601702c57bb3a03ebceca8095836d8451d2
RodrigoGasparSeptiembre <92555965+RodrigoGasparSeptiembre@users.noreply.github.com>
2021-12-16 21:06:40 -0300

    Add files via upload

--- models.py
+++ models.py
@@ -1,13 +1,23 @@
 from django.db import models
 
 # Create your models here.
-class persona(models.model):
-    id = models.autofield(primary_key=true)
-    nombre= models.charfield(max_lenght=60)
-    apellido= models.charfield(max_lenght=100)
-    club= models.charfield(max_lenght=50)
+class Persona(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=60)
+    apellido = models.CharField(max_length=100)
+    club = models.CharField(max_length=50)
+    foto = models.ImageField(
+        upload_to = 'foto/%Y/%m/%d',
+        blank = True,
+        verbose_name = ('Foto de la persona')
+    )
+    dni = models.CharField(max_length=50,blank=True)
+
+
 class Meta:
-    verbose_name= ('Persona')
-    verbose_name_plural= ('Personas')
-    def __str__(self):
+    verbose_name = ('Persona')
+    verbose_name_plural = ('Personas')
+
+def __str__(self):
     return self.nombre
